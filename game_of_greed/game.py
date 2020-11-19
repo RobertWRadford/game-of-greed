f0394c6865ce862ceabb61ad26e383d06f4630ce
Robert Radford <RobertR928@gmail.com>
2020-11-18 21:55:24 -0500

    segmented functionality

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -1,3 +1,4 @@
+import sys
 from game_of_greed.game_logic import Banker, GameLogic
 
 
@@ -34,73 +35,87 @@ class Game:
     def decline_game(self):
         print("OK. Maybe another time")
 
+    def new_roll(self, session, game_round, remaining_die):
+        print(f"Rolling {remaining_die} dice...")
+        die_roll = session.roll_dice(remaining_die)
+        string_roll = [str(int) for int in die_roll]
+        return (die_roll, string_roll)
 
+    def check_validity(self, point_total, die_roll, string_roll):
+        cheated = True
+        while cheated:
+            cheated = False
+            print(f'*** {" ".join(string_roll)} ***')
+            print("Enter dice to keep, or (q)uit:")
+            kept_die = input("> ")
+            if kept_die == "q":
+                print(f"Thanks for playing. You earned {point_total} points")
+                sys.exit()
+
+            good_input = True
+            for value in kept_die:
+                try:
+                    int(value)
+                except:
+                    good_input = False
+            if good_input == False:
+                continue
+            user_selection = tuple(int(char) for char in kept_die)
+            for value in user_selection:
+                if value not in die_roll:
+                    cheated = True
+            if cheated:
+                print('Cheater!!! Or possibly made a typo...')
+        return user_selection
+
+    def round_decisions(self, session, game_round, point_total, round_points, shelf_points, unbanked_points, remaining_die):
+        print(f"You have {unbanked_points} unbanked points and {remaining_die} dice remaining")
+        print("(r)oll again, (b)ank your points or (q)uit:")
+        round_choice = input("> ")
+        if round_choice == "q":
+            print(f"Thanks for playing. You earned {point_total} points")
+            sys.exit()
+        elif round_choice == "b":
+            point_total += round_points
+            remaining_die = 6
+            print(f"You banked {round_points} points in round {game_round}")
+        elif round_choice == "r":
+            point_total += shelf_points
+            if remaining_die == 0:
+                remaining_die = 6
+            self.full_roll(session, game_round, point_total, remaining_die)
+
+        return(point_total, round_points, shelf_points, unbanked_points, remaining_die)
+
+    def full_roll(self, session, game_round, point_total, remaining_die):
+        
+        (die_roll, string_roll) = self.new_roll(session, game_round, remaining_die)
+        user_selection = self.check_validity(point_total, die_roll, string_roll)
+
+        remaining_die -= len(user_selection)
+        round_points = session.calculate_score(die_roll)
+        shelf_points = session.calculate_score(user_selection)
+        unbanked_points = session.calculate_score(set(die_roll)- set(user_selection))
+
+        if round_points == 0:
+            print('****************************************\n**        Zilch!!! Round over         **\n****************************************')
+            print(f"You banked {round_points} points in round {game_round}")
+        else:
+            (point_total, round_points, shelf_points, unbanked_points, remaining_die) = self.round_decisions(session, game_round, point_total, round_points, shelf_points, unbanked_points, remaining_die)
+            return point_total
 
     def start_game(self):
         session = GameLogic()
         game_round = 0
         point_total = 0
-        remaing_die = 6 
+        remaining_die = 6 
         
         while point_total< 10000:
             game_round += 1
             print(f"Starting round {game_round}")
-            print(f"Rolling {remaing_die} dice...")
-<<<<<<< HEAD
-            die_roll = session.roll_dice(remaing_die)
-            string_roll = [str(int) for int in die_roll]
-            print(f'*** {" ".join(string_roll)} ***')
-=======
-            die_roll = session.roll_dice(remaing_die)  
-            string_roll = [str(int) for int in die_roll]
-            print(f'*** {string_roll} ***')
->>>>>>> 9bea4cb31d828044f2d6a524a710154456615efa
-            print("Enter dice to keep, or (q)uit:")
-            kept_die = input("> ")
-            if kept_die == "q":
-                print(f"Thanks for playing. You earned {point_total} points")
-                break
-            user_selection = tuple(int(char)for char in kept_die)
-            remaing_die -= len(user_selection)
-            shelf_points = session.calculate_score(user_selection)
-            round_points = session.calculate_score(die_roll)
-            unbanked_points = session.calculate_score(set(die_roll)- set(user_selection))
-            if round_points == 0:
-                print('****************************************\n**        Zilch!!! Round over         **\n****************************************')
-                print(f"You banked {round_points} points in round {game_round}")
-            else:
-                print(f"You have {unbanked_points} unbanked points and {remaing_die} dice remaining")
-                print("(r)oll again, (b)ank your points or (q)uit:")
-                round_choice = input("> ")
-                if round_choice == "q":
-                    print(f"Thanks for playing. You earned {point_total} points")
-                    break
-                elif round_choice == "b":
-                    point_total += round_points
-                    remaing_die = 6
-                    print(f"You banked {round_points} points in round {game_round}")
-                elif round_choice == "r":
-                    point_total += shelf_points
+            point_total = self.full_roll(session, game_round, point_total, remaining_die)
             print(f"Total score is {point_total} points")
-<<<<<<< HEAD
-                    
-=======
 
-                    
-
-
-
-
-
-
-
-
-
-
-        
-
-
->>>>>>> 9bea4cb31d828044f2d6a524a710154456615efa
 if __name__ == "__main__":
     game = Game()
     game.play()
